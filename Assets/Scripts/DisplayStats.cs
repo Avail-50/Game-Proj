6d1153e047a337a75f7cbc770f913343e9959319
Avail-50 <apontidas@gmail.com>
2023-12-11 21:14:19 +0000

    whky is it brakingD8io7gfwuiohughre

--- Assets/Scripts/DisplayStats.cs
+++ Assets/Scripts/DisplayStats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Display : MonoBehaviour
+public class DisplayStats : MonoBehaviour
 {
     public GameObject player;
     private playerControl speed;
